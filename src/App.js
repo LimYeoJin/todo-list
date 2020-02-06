0861e2d5fccf83bd1c6f8b2824c66b3a36cf21dd
LimYeoJin <wlsxor5828@naver.com>
2020-02-06 20:51:21 +0900

    'Item'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TodoListTemplate from './Components/TodoListTemplate';
 import Form from './Components/Form';
+import TodoItemList from './Components/TodoItemList';
 
 function App() {
   return (
-    <TodoListTemplate form={<Form/>}> 
+    <TodoListTemplate form={<Form/>} children={<TodoItemList/>}> 
       
     </TodoListTemplate>
   );
